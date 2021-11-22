c292ca3f40d911e376732f22ab90050b852f8696
awned <65061212+ceilingfans@users.noreply.github.com>
2021-11-22 19:52:41 +0800

    chore: made getters take only 1 line

--- src/Token.cpp
+++ src/Token.cpp
@@ -6,18 +6,8 @@ namespace Fyt
 {
 	Token::Token(TokenType p_type, TokenName p_name, std::string p_literal) : type(p_type), name(p_name), literal(std::move(p_literal)) {}
 
-	TokenType Token::getType() const
-	{
-		return type;
-	}
+	TokenType Token::getType() const { return type; }
+	TokenName Token::getName() const { return name; }
+	std::string Token::getLiteral() const { return literal; }
 
-	TokenName Token::getName() const
-	{
-		return name;
-	}
-
-	std::string Token::getLiteral() const
-	{
-		return literal;
-	}
 } /* namespace Fyt */
