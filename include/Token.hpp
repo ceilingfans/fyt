2413624db665dc7fcdfbec01d22e8ad8d55d2925
awned <65061212+ceilingfans@users.noreply.github.com>
2021-11-26 12:04:52 +0800

    refactor: remove redundant TokenName enum class and implementations

--- include/Token.hpp
+++ include/Token.hpp
@@ -34,32 +34,17 @@ namespace Fyt
 		"COMMENT",
 	};
 
-	enum class TokenName
-	{
-		INVALID = -1,
-		OP_ARITHMETIC_PLUS,
-		OP_ARITHMETIC_MINUS,
-		OP_ARITHMETIC_DIVIDE,
-		OP_ARITHMETIC_MULTIPLY,
-		NUMBER_INTEGER32,
-		NUMBER_DOUBLE,
-	};
-
 	class Token
 	{
 	public:
-		Token(TokenType p_type, TokenName p_name, std::string p_literal);
 		Token();
 		TokenType getType() const;
-		TokenName getName() const;
 		std::string getLiteral() const;
 		void setType(TokenType p_type);
-		void setName(TokenName p_name);
 		void setLiteral(std::string p_literal);
 		void print();
 	private:
 		TokenType type { TokenType::INVALID };
-		TokenName name { TokenName::INVALID };
 		std::string literal;
 	};
 } /* namespace Fyt */
--- src/Token.cpp
+++ src/Token.cpp
@@ -10,17 +10,14 @@ namespace Fyt
 		return static_cast<typename std::underlying_type<E>::type>(e);
 	}
 
-	Token::Token(TokenType p_type, TokenName p_name, std::string p_literal) : type(p_type), name(p_name), literal(std::move(p_literal)) {}
 	Token::Token() = default;
 
 	/* Getters */
 	TokenType Token::getType() const { return type; }
-	TokenName Token::getName() const { return name; }
 	std::string Token::getLiteral() const { return literal; }
 
 	/* Setters */
 	void Token::setType(TokenType p_type) { type = p_type; }
-	void Token::setName(TokenName p_name) { name = p_name; }
 	void Token::setLiteral(std::string p_literal) { literal = std::move(p_literal); }
 
 	void Token::print() {
